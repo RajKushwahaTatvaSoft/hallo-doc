9151043d991c1d9afbc2c753183408e43fb9734a
Raj Kushwaha <PCA195@server7.com>
2024-01-19 18:30:12 +0530

    added all four requests pages with responsive design

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,14 @@
 include('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
+
+var isLightMode = true
+
+function myFunction () {
+    console.log("helo body");
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    isLightMode = !isLightMode
+
+    
+    // var img = element.querySelector(".lightmode")
+    // img.src.includes("darkmode") ? img.src = "../../images/lightmode.png" : img.src = "../../images/darkmode.png";
+}
--- js/patient.js
+++ js/patient.js
@@ -1,1 +1,9 @@
-include('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
+var isLightMode = true
+
+function myFunction() {
+    console.log("helo body");
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    isLightMode = !isLightMode
+
+}
